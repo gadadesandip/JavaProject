7c5c1ab7c992dd61ee48a68b4359b6f13c17290b
gadadesandip <gadade.sandip11@gmail.com>
2022-12-26 23:07:44 +0530

    first commit

---
+++ src/com/linked/LinkedList.java
@@ -0,0 +1,20 @@
+package com.linked;
+
+import java.util.ArrayList;
+
+public class LinkedList {
+
+	public static void main(String[] args) {
+ArrayList al= new ArrayList();
+		
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		
+		for (Object e:al) {
+			System.out.println(e);
+
+	}
+
+}
+}
